5911eb61d5d92daf0582637a22481d6387d8094c
mntcre <maeum@gmx.de>
2021-09-23 18:04:34 +0200

    new header, responsive grid, footer

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-gsap.from("#tri", {
-  x: 500,
-  opacity: 0
+gsap.from("#para09", {
+  x: 1000,
+  duration: 1
 });
+gsap.from("#para08", {
+  x: 1000,
+  duration: 1.5
+});
+
+gsap.from("#para07", {
+  x: 1000,
+  duration: 1.4
+});
+gsap.from("#para06", {
+  x: 1000,
+  duration: 0.9
+});
+gsap.from("#para05", {
+  x: 1000,
+  duration: 0.8
+});
+gsap.from("#para04", {
+  x: 1000,
+  duration: 0.9
+});
+gsap.from("#para01", {
+  x: 1000,
+  duration: 1.4
+});
+
+gsap.from("#para03", {
+  x: -500,
+  duration: 0.6
+});
+gsap.from("#para02", {
+  x: -500,
+  duration: 1.4
+});
+
+
+
 gsap.from("#head1", {
   x: -500,
-  opacity: 0
+  opacity: 0,
+  duration: 1.5
 });
